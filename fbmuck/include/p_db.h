fuzzball-muck/fuzzball

commit c8c67821b892a3ca7ae927fb0704cbcdf381de06

Added the primitive ENTRANCES_ARRAY from Proto. [fr #67] Committed HTML versions of previously-changed help files.

fbmuck/include/p_db.h
@@ -56,6 +56,7 @@ extern void prim_newprogram(PRIM_PROTOTYPE);
 extern void prim_contents_array(PRIM_PROTOTYPE);
 extern void prim_exits_array(PRIM_PROTOTYPE);
 extern void prim_getlinks_array(PRIM_PROTOTYPE);
+extern void prim_entrances_array(PRIM_PROTOTYPE);
 extern void prim_newpassword(PRIM_PROTOTYPE);
 extern void prim_compile(PRIM_PROTOTYPE);
 extern void prim_uncompile(PRIM_PROTOTYPE);
@@ -68,7 +69,7 @@ extern void prim_program_setlines(PRIM_PROTOTYPE);
 /* WORK: Add these prims */
 extern void prim_toadplayer(PRIM_PROTOTYPE);
 
-#define PRIMS_DB_FUNCS1 prim_addpennies, prim_moveto, prim_pennies,       \
+#define PRIMS_DB_FUNCS1 prim_addpennies, prim_moveto, prim_pennies,      \
     prim_dbcomp, prim_dbref, prim_contents, prim_exits, prim_next,       \
     prim_name, prim_setname, prim_match, prim_rmatch, prim_copyobj,      \
     prim_set, prim_mlevel, prim_flagp, prim_playerp, prim_thingp,        \
@@ -76,18 +77,18 @@ extern void prim_toadplayer(PRIM_PROTOTYPE);
     prim_owner, prim_getlink, prim_setlink, prim_setown, prim_newobject, \
     prim_newroom, prim_newexit, prim_lockedp, prim_recycle,              \
     prim_setlockstr, prim_getlockstr, prim_part_pmatch, prim_controls,   \
-    prim_checkpassword, prim_nextowned, prim_getlinks,    \
+    prim_checkpassword, prim_nextowned, prim_getlinks,                   \
     prim_pmatch, prim_movepennies, prim_findnext, prim_nextentrance,     \
-	prim_newplayer, prim_copyplayer, prim_objmem, prim_instances,        \
-	prim_compiledp, prim_newprogram, prim_contents_array,                \
-	prim_exits_array, prim_getlinks_array, prim_compile,                 \
-	prim_uncompile, prim_newpassword, prim_getpids,                      \
-	prim_program_getlines, prim_getpidinfo, prim_program_setlines,		 \
-	prim_setlinks_array
+    prim_newplayer, prim_copyplayer, prim_objmem, prim_instances,        \
+    prim_compiledp, prim_newprogram, prim_contents_array,                \
+    prim_exits_array, prim_getlinks_array, prim_entrances_array,         \
+    prim_compile, prim_uncompile, prim_newpassword, prim_getpids,        \
+    prim_program_getlines, prim_getpidinfo, prim_program_setlines,	 \
+    prim_setlinks_array
 
 
 
-#define PRIMS_DB_NAMES1 "ADDPENNIES", "MOVETO", "PENNIES",  \
+#define PRIMS_DB_NAMES1 "ADDPENNIES", "MOVETO", "PENNIES", \
     "DBCMP", "DBREF", "CONTENTS", "EXITS", "NEXT",         \
     "NAME", "SETNAME", "MATCH", "RMATCH", "COPYOBJ",       \
     "SET", "MLEVEL", "FLAG?", "PLAYER?", "THING?",         \
@@ -95,14 +96,14 @@ extern void prim_toadplayer(PRIM_PROTOTYPE);
     "OWNER", "GETLINK", "SETLINK", "SETOWN", "NEWOBJECT",  \
     "NEWROOM", "NEWEXIT", "LOCKED?", "RECYCLE",            \
     "SETLOCKSTR", "GETLOCKSTR", "PART_PMATCH", "CONTROLS", \
-    "CHECKPASSWORD", "NEXTOWNED", "GETLINKS",  \
+    "CHECKPASSWORD", "NEXTOWNED", "GETLINKS",              \
     "PMATCH", "MOVEPENNIES", "FINDNEXT", "NEXTENTRANCE",   \
-	"NEWPLAYER", "COPYPLAYER", "OBJMEM", "INSTANCES",      \
-	"COMPILED?", "NEWPROGRAM", "CONTENTS_ARRAY",           \
-	"EXITS_ARRAY", "GETLINKS_ARRAY", "COMPILE",            \
-	"UNCOMPILE", "NEWPASSWORD", "GETPIDS",                 \
-	"PROGRAM_GETLINES", "GETPIDINFO", "PROGRAM_SETLINES",  \
-	"SETLINKS_ARRAY"
+    "NEWPLAYER", "COPYPLAYER", "OBJMEM", "INSTANCES",      \
+    "COMPILED?", "NEWPROGRAM", "CONTENTS_ARRAY",           \
+    "EXITS_ARRAY", "GETLINKS_ARRAY", "ENTRANCES_ARRAY",    \
+    "COMPILE", "UNCOMPILE", "NEWPASSWORD", "GETPIDS",      \
+    "PROGRAM_GETLINES", "GETPIDINFO", "PROGRAM_SETLINES",  \
+    "SETLINKS_ARRAY"
 
 #define PRIMS_DB_CNT1 60
 

fbmuck/docs/mpihelp.html
@@ -427,7 +427,9 @@ like &quot;9d 12:56&quot; for 9 days, 12 hours, and 56 minutes.
 </a></h4>
     Given a time period, in seconds, this will return a string, including a
 breakdown of all the time units of that period.  For example, given a number
-of seconds, it might return &quot;1 week, 2 days, 10 mins, 52 secs&quot;.
+of seconds, it might return:
+<p>
+        3 years, 10 months, 4 weeks, 1 day, 18 hours, 40 mins, 21 secs
 <p>
 
 

fbmuck/docs/muckhelp.html
@@ -17,76 +17,77 @@
 <p><hr size="6"><h3><a name="AlphaList">Alphabetical List of Topics</a></h3>
 <h4>Symbols</h4><blockquote><table border=0>
   <tr>
+    <td nowrap> &nbsp; <a href="#@@">@@</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@action">@action</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@armageddon">@armageddon</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@attach">@attach</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@bless">@bless</a> &nbsp; </td>
-    <td nowrap> &nbsp; <a href="#@boot">@boot</a> &nbsp; </td>
   </tr>
   <tr>
+    <td nowrap> &nbsp; <a href="#@boot">@boot</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@chown">@chown</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@chown_lock">@chown_lock</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@contents">@contents</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@create">@create</a> &nbsp; </td>
-    <td nowrap> &nbsp; <a href="#@delta">@delta</a> &nbsp; </td>
   </tr>
   <tr>
+    <td nowrap> &nbsp; <a href="#@delta">@delta</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@describe">@describe</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@dig">@dig</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@drop">@drop</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@dump">@dump</a> &nbsp; </td>
-    <td nowrap> &nbsp; <a href="#@edit">@edit</a> &nbsp; </td>
   </tr>
   <tr>
+    <td nowrap> &nbsp; <a href="#@edit">@edit</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@entrances">@entrances</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@fail">@fail</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@find">@find</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@force">@force</a> &nbsp; </td>
-    <td nowrap> &nbsp; <a href="#@idescribe">@idescribe</a> &nbsp; </td>
   </tr>
   <tr>
+    <td nowrap> &nbsp; <a href="#@idescribe">@idescribe</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@kill">@kill</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@link">@link</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@list">@list</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@lock">@lock</a> &nbsp; </td>
-    <td nowrap> &nbsp; <a href="#@mcpedit">@mcpedit</a> &nbsp; </td>
   </tr>
   <tr>
+    <td nowrap> &nbsp; <a href="#@mcpedit">@mcpedit</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@mcpprogram">@mcpprogram</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@name">@name</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@newpassword">@newpassword</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@odrop">@odrop</a> &nbsp; </td>
-    <td nowrap> &nbsp; <a href="#@ofail">@ofail</a> &nbsp; </td>
   </tr>
   <tr>
+    <td nowrap> &nbsp; <a href="#@ofail">@ofail</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@open">@open</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@osuccess">@osuccess</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@owned">@owned</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@password">@password</a> &nbsp; </td>
-    <td nowrap> &nbsp; <a href="#@pcreate">@pcreate</a> &nbsp; </td>
   </tr>
   <tr>
+    <td nowrap> &nbsp; <a href="#@pcreate">@pcreate</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@program">@program</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@propset">@propset</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@ps">@ps</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@recycle">@recycle</a> &nbsp; </td>
-    <td nowrap> &nbsp; <a href="#@restart">@restart</a> &nbsp; </td>
   </tr>
   <tr>
+    <td nowrap> &nbsp; <a href="#@restart">@restart</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@restrict">@restrict</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@set">@set</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@shutdown">@shutdown</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@stats">@stats</a> &nbsp; </td>
-    <td nowrap> &nbsp; <a href="#@success">@success</a> &nbsp; </td>
   </tr>
   <tr>
+    <td nowrap> &nbsp; <a href="#@success">@success</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@teleport">@teleport</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@toad">@toad</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@trace">@trace</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@unbless">@unbless</a> &nbsp; </td>
-    <td nowrap> &nbsp; <a href="#@unlink">@unlink</a> &nbsp; </td>
   </tr>
   <tr>
+    <td nowrap> &nbsp; <a href="#@unlink">@unlink</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@unlock">@unlock</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@usage">@usage</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#@wall">@wall</a> &nbsp; </td>
@@ -3006,6 +3007,7 @@ with a corrupted database, it will be with @restricted logins.
 <h3><a name="MiscHelp">Miscellaneous</a></h3>
 <blockquote><table border=0>
   <tr>
+    <td width="20%"> &nbsp; <a href="#@@">@@</a> &nbsp; </td>
     <td width="20%"> &nbsp; <a href="#cheatsheet">cheatsheet</a> &nbsp; </td>
     <td width="20%"> &nbsp; <a href="#costs">costs</a> &nbsp; </td>
     <td width="20%"> &nbsp; <a href="#outputprefix">outputprefix</a> &nbsp; </td>
@@ -3097,6 +3099,31 @@ robots may use this command.
 <p><h5>Also see:
     <a href="#outputprefix">OUTPUTPREFIX</a>
 </h5>
+<p>
+
+
+<hr><h4><a name="@@">@@
+<br>
+
+<br>
+The '@@' command is a NULL (do-nothing) built-in server command: it is not
+<br>
+evaluated/matched and is simply ignored by the MUCK server. Yet, it does reset
+<br>
+your idle timer, thus being a good candidate as a &quot;ping&quot; command to avoid being
+<br>
+booted for inactivity. Sending regularly this command (every 10 minutes or so)
+<br>
+is also a good means to avoid seeing the connection closed for inactivity by the
+<br>
+routers on the path between your computer and the MUCK server. Many MUCK clients
+<br>
+do allow to send an arbitrary command on a regular basis to the MUCKs: use '@@'
+<br>
+for such an arbitrary command (it should also be noted that this command will
+<br>
+also work for anti-idling purposes on MUSHes and MUXes).
+<br>
 <p>
 
 

fbmuck/docs/mufman.html
@@ -380,20 +380,23 @@
 <h4>E's</h4><blockquote><table border=0>
   <tr>
     <td nowrap> &nbsp; <a href="#else">else</a> &nbsp; </td>
+    <td nowrap> &nbsp; <a href="#entrances_array">entrances_array</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#envprop">envprop</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#envpropstr">envpropstr</a> &nbsp; </td>
-    <td nowrap> &nbsp; <a href="#epsilon">epsilon</a> &nbsp; </td>
-    <td nowrap> &nbsp; <a href="#error?">error?</a> &nbsp; </td>
   </tr>
   <tr>
+    <td nowrap> &nbsp; <a href="#epsilon">epsilon</a> &nbsp; </td>
+    <td nowrap> &nbsp; <a href="#error?">error?</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#error_bit">error_bit</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#error_name">error_name</a> &nbsp; </td>
+  </tr>
+  <tr>
     <td nowrap> &nbsp; <a href="#error_num">error_num</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#error_str">error_str</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#event_count">event_count</a> &nbsp; </td>
+    <td nowrap> &nbsp; <a href="#event_exists">event_exists</a> &nbsp; </td>
   </tr>
   <tr>
-    <td nowrap> &nbsp; <a href="#event_exists">event_exists</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#event_send">event_send</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#event_wait">event_wait</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#event_waitfor">event_waitfor</a> &nbsp; </td>
@@ -404,9 +407,9 @@
     <td nowrap> &nbsp; <a href="#exit?">exit?</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#exits">exits</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#exits_array">exits_array</a> &nbsp; </td>
-    <td nowrap> &nbsp; <a href="#exp">exp</a> &nbsp; </td>
   </tr>
   <tr>
+    <td nowrap> &nbsp; <a href="#exp">exp</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#explode">explode</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#explode_array">explode_array</a> &nbsp; </td>
     <td nowrap> &nbsp; <a href="#ext-name-ok?">ext-name-ok?</a> &nbsp; </td>
@@ -2266,7 +2269,7 @@ that triggered the the program, without the command line arguments.  ie:
 if there was an exit named &quot;abracadabra;foo bar;frozzboz&quot; that was linked
 to the program, and the user typed in &quot;foo bar baz&quot;, then the program
 would run with &quot;baz&quot; on the stack, and &quot;foo bar&quot; in the global COMMAND
-variable.  The TRIGGER global variable is the same variable as '2 variable'
+variable.  The COMMAND global variable is the same variable as '3 variable'
 <p><h5>Also see:
     <a href="#trigger">TRIGGER</a>,
     <a href="#loc">LOC</a> and
@@ -6152,9 +6155,12 @@ property s in object d. If the property is cleared, 0 is returned.
 </a></h4>
   Takes a starting object dbref and a property name and searches down the
 environment tree from that object for a property with the given name.  If
-the property isn't found, it returns #-1 and a null string.  If the
-property is found, it will return the dbref of the object it was found on,
-and the value it contained.
+the property isn't found, it returns #-1 and 0.  If the property is found,
+it will return the dbref of the object it was found on, and the value it
+contained.
+<p><h5>Also see:
+    <a href="#envpropstr">ENVPROPSTR</a>
+</h5>
 <p>
 
 
@@ -6168,6 +6174,9 @@ environment tree from that object for a property with the given name.  If
 the property isn't found, it returns #-1 and a null string.  If the
 property is found, it will return the dbref of the object it was found on,
 and the string value it contained.
+<p><h5>Also see:
+    <a href="#envprop">ENVPROP</a>
+</h5>
 <p>
 
 
@@ -6471,114 +6480,115 @@ This primitives requires at least mucker level 3.
   <tr>
     <td width="33%"> &nbsp; <a href="#desc">desc</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#drop">drop</a> &nbsp; </td>
-    <td width="33%"> &nbsp; <a href="#exit?">exit?</a> &nbsp; </td>
+    <td width="33%"> &nbsp; <a href="#entrances_array">entrances_array</a> &nbsp; </td>
   </tr>
   <tr>
+    <td width="33%"> &nbsp; <a href="#exit?">exit?</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#exits">exits</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#exits_array">exits_array</a> &nbsp; </td>
-    <td width="33%"> &nbsp; <a href="#fail">fail</a> &nbsp; </td>
   </tr>
   <tr>
+    <td width="33%"> &nbsp; <a href="#fail">fail</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#findnext">findnext</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#flag?">flag?</a> &nbsp; </td>
-    <td width="33%"> &nbsp; <a href="#getlink">getlink</a> &nbsp; </td>
   </tr>
   <tr>
+    <td width="33%"> &nbsp; <a href="#getlink">getlink</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#getlinks">getlinks</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#getlinks_array">getlinks_array</a> &nbsp; </td>
-    <td width="33%"> &nbsp; <a href="#ignore_add">ignore_add</a> &nbsp; </td>
   </tr>
   <tr>
+    <td width="33%"> &nbsp; <a href="#ignore_add">ignore_add</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#ignore_del">ignore_del</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#ignoring?">ignoring?</a> &nbsp; </td>
-    <td width="33%"> &nbsp; <a href="#location">location</a> &nbsp; </td>
   </tr>
   <tr>
+    <td width="33%"> &nbsp; <a href="#location">location</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#match">match</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#mlevel">mlevel</a> &nbsp; </td>
-    <td width="33%"> &nbsp; <a href="#movepennies">movepennies</a> &nbsp; </td>
   </tr>
   <tr>
+    <td width="33%"> &nbsp; <a href="#movepennies">movepennies</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#moveto">moveto</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#name">name</a> &nbsp; </td>
-    <td width="33%"> &nbsp; <a href="#newexit">newexit</a> &nbsp; </td>
   </tr>
   <tr>
+    <td width="33%"> &nbsp; <a href="#newexit">newexit</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#newobject">newobject</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#newpassword">newpassword</a> &nbsp; </td>
-    <td width="33%"> &nbsp; <a href="#newplayer">newplayer</a> &nbsp; </td>
   </tr>
   <tr>
+    <td width="33%"> &nbsp; <a href="#newplayer">newplayer</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#newprogram">newprogram</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#newroom">newroom</a> &nbsp; </td>
-    <td width="33%"> &nbsp; <a href="#next">next</a> &nbsp; </td>
   </tr>
   <tr>
+    <td width="33%"> &nbsp; <a href="#next">next</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#nextentrance">nextentrance</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#nextowned">nextowned</a> &nbsp; </td>
-    <td width="33%"> &nbsp; <a href="#objmem">objmem</a> &nbsp; </td>
   </tr>
   <tr>
+    <td width="33%"> &nbsp; <a href="#objmem">objmem</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#odrop">odrop</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#ofail">ofail</a> &nbsp; </td>
-    <td width="33%"> &nbsp; <a href="#ok?">ok?</a> &nbsp; </td>
   </tr>
   <tr>
+    <td width="33%"> &nbsp; <a href="#ok?">ok?</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#osucc">osucc</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#owner">owner</a> &nbsp; </td>
-    <td width="33%"> &nbsp; <a href="#part_pmatch">part_pmatch</a> &nbsp; </td>
   </tr>
   <tr>
+    <td width="33%"> &nbsp; <a href="#part_pmatch">part_pmatch</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#pennies">pennies</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#player?">player?</a> &nbsp; </td>
-    <td width="33%"> &nbsp; <a href="#pmatch">pmatch</a> &nbsp; </td>
   </tr>
   <tr>
+    <td width="33%"> &nbsp; <a href="#pmatch">pmatch</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#prog">prog</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#program?">program?</a> &nbsp; </td>
-    <td width="33%"> &nbsp; <a href="#recycle">recycle</a> &nbsp; </td>
   </tr>
   <tr>
+    <td width="33%"> &nbsp; <a href="#recycle">recycle</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#rmatch">rmatch</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#room?">room?</a> &nbsp; </td>
-    <td width="33%"> &nbsp; <a href="#set">set</a> &nbsp; </td>
   </tr>
   <tr>
+    <td width="33%"> &nbsp; <a href="#set">set</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#setdesc">setdesc</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#setdrop">setdrop</a> &nbsp; </td>
-    <td width="33%"> &nbsp; <a href="#setfail">setfail</a> &nbsp; </td>
   </tr>
   <tr>
+    <td width="33%"> &nbsp; <a href="#setfail">setfail</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#setlink">setlink</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#setlinks_array">setlinks_array</a> &nbsp; </td>
-    <td width="33%"> &nbsp; <a href="#setname">setname</a> &nbsp; </td>
   </tr>
   <tr>
+    <td width="33%"> &nbsp; <a href="#setname">setname</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#setodrop">setodrop</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#setofail">setofail</a> &nbsp; </td>
-    <td width="33%"> &nbsp; <a href="#setosucc">setosucc</a> &nbsp; </td>
   </tr>
   <tr>
+    <td width="33%"> &nbsp; <a href="#setosucc">setosucc</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#setown">setown</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#setsucc">setsucc</a> &nbsp; </td>
-    <td width="33%"> &nbsp; <a href="#setsysparm">setsysparm</a> &nbsp; </td>
   </tr>
   <tr>
+    <td width="33%"> &nbsp; <a href="#setsysparm">setsysparm</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#stats">stats</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#succ">succ</a> &nbsp; </td>
-    <td width="33%"> &nbsp; <a href="#sysparm">sysparm</a> &nbsp; </td>
   </tr>
   <tr>
+    <td width="33%"> &nbsp; <a href="#sysparm">sysparm</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#sysparm_array">sysparm_array</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#thing?">thing?</a> &nbsp; </td>
-    <td width="33%"> &nbsp; <a href="#timestamps">timestamps</a> &nbsp; </td>
   </tr>
   <tr>
+    <td width="33%"> &nbsp; <a href="#timestamps">timestamps</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#toadplayer">toadplayer</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#trig">trig</a> &nbsp; </td>
-    <td width="33%"> &nbsp; <a href="#truename">truename</a> &nbsp; </td>
   </tr>
   <tr>
+    <td width="33%"> &nbsp; <a href="#truename">truename</a> &nbsp; </td>
     <td width="33%"> &nbsp; <a href="#unparseobj">unparseobj</a> &nbsp; </td>
   </tr>
 </table></blockquote>
@@ -7658,6 +7668,16 @@ or a room with no dropto, getlinks returns 0.  A count of
 </a></h4>
   Returns a list array that contains the dbref of every object that the
 given object is linked to.
+<p>
+
+
+<hr><h4><a name="entrances_array">ENTRANCES_ARRAY ( d -- a )
+<br>
+
+<br>
+</a></h4>
+  Returns a list array that contains the dbref of every object linked to the
+given object.
 <p>
 
 

fbmuck/game/data/mufman.raw
@@ -4127,6 +4127,13 @@ GETLINKS_ARRAY ( d -- a )
 given object is linked to.
 ~
 ~
+ENTRANCES_ARRAY
+ENTRANCES_ARRAY ( d -- a )
+
+  Returns a list array that contains the dbref of every object linked to the
+given object.
+~
+~
 SETLINK
 SETLINK     ( d1 d2 -- )
 

fbmuck/src/p_db.c
@@ -2310,6 +2310,52 @@ prim_getlinks_array(PRIM_PROTOTYPE)
 }
 
 void
+prim_entrances_array(PRIM_PROTOTYPE)
+{
+    stk_array *nw;
+    int count = 0;
+    dbref i, j;
+
+    CHECKOP(1);
+    oper1 = POP();
+
+    if (!valid_object(oper1))
+        abort_interp("Invalid object dbref. (1)");
+
+    ref = oper1->data.objref;
+    nw = new_array_packed(0);
+
+    for (i = 0; i < db_top; i++) {
+        switch (Typeof(i)) {
+            case TYPE_EXIT:
+                for (j = DBFETCH(i)->sp.exit.ndest; j--;) {
+                    if (DBFETCH(i)->sp.exit.dest[j] == ref)
+			array_set_intkey_refval(&nw, count++, i);
+                }
+                break;
+            case TYPE_PLAYER:
+                if (PLAYER_HOME(i) == ref)
+		    array_set_intkey_refval(&nw, count++, i);
+                break;
+            case TYPE_THING:
+                if (THING_HOME(i) == ref)
+		    array_set_intkey_refval(&nw, count++, i);
+                break;
+            case TYPE_ROOM:
+                if (DBFETCH(i)->sp.room.dropto == ref)
+		    array_set_intkey_refval(&nw, count++, i);
+                break;
+            case TYPE_PROGRAM:
+            case TYPE_GARBAGE:
+                break;
+        }
+    }
+
+    CLEAR(oper1);
+    PushArrayRaw(nw);
+}
+
+void
 prim_program_getlines(PRIM_PROTOTYPE)
 {
 	stk_array* ary;

commit 740db3a170ec296dde122dd827a23e4494b90752

Fixing crasher introduced in c8c67821b892a3ca7ae927fb0704cbcdf381de06 -- primitive counts must match the number defined

fbmuck/include/p_db.h
@@ -105,7 +105,7 @@ extern void prim_toadplayer(PRIM_PROTOTYPE);
     "PROGRAM_GETLINES", "GETPIDINFO", "PROGRAM_SETLINES",  \
     "SETLINKS_ARRAY"
 
-#define PRIMS_DB_CNT1 60
+#define PRIMS_DB_CNT1 61
 
 
 #ifdef SCARY_MUF_PRIMS