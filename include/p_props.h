ef093efa1d1cb015155f9b8045d983b5172546af
wyld-sw <wyld@fuzzball.org>
2016-08-03 10:09:39 -0400

    Added PARSEMPI (mostly from Proto) and PARSEMPIBLESSED.

--- include/p_props.h
+++ include/p_props.h
@@ -24,21 +24,24 @@ void prim_reflist_add(PRIM_PROTOTYPE);
 void prim_reflist_del(PRIM_PROTOTYPE);
 void prim_blessedp(PRIM_PROTOTYPE);
 void prim_parsepropex(PRIM_PROTOTYPE);
+void prim_parsempi(PRIM_PROTOTYPE);
+void prim_parsempiblessed(PRIM_PROTOTYPE);
 
 #define PRIMS_PROPS_FUNCS prim_getpropval, prim_getpropstr, prim_remove_prop, \
    prim_envprop, prim_envpropstr, prim_addprop, prim_nextprop, prim_propdirp, \
    prim_parseprop, prim_getprop, prim_setprop, prim_getpropfval, \
    prim_blessprop, prim_unblessprop, prim_array_filter_prop, \
    prim_reflist_find, prim_reflist_add, prim_reflist_del, prim_blessedp, \
-   prim_parsepropex
+   prim_parsepropex, prim_parsempi, prim_parsempiblessed
 
 #define PRIMS_PROPS_NAMES "GETPROPVAL", "GETPROPSTR", "REMOVE_PROP",  \
     "ENVPROP", "ENVPROPSTR", "ADDPROP", "NEXTPROP", "PROPDIR?", \
     "PARSEPROP", "GETPROP", "SETPROP", "GETPROPFVAL", \
 	"BLESSPROP", "UNBLESSPROP", "ARRAY_FILTER_PROP", \
 	"REFLIST_FIND", "REFLIST_ADD", "REFLIST_DEL", "BLESSED?", \
-	"PARSEPROPEX"
+	"PARSEPROPEX", "PARSEMPI", "PARSEMPIBLESSED"
 
-#define PRIMS_PROPS_CNT 20
+#define PRIMS_PROPS_CNT 22
 
 #endif				/* _P_PROPS_H */
+
