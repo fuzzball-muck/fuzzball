ed208268069c77d6e5ad2df554d1497a39b0ebf6
wyld-sw <wyld@fuzzball.org>
2016-08-27 08:56:50 -0400

    Added REGSPLIT and REGSPLIT_NOEMPTY, to split a string via a regex pattern.

--- include/p_regex.h
+++ include/p_regex.h
@@ -1,15 +1,18 @@
 #ifndef _P_REGEX_H
 #define _P_REGEX_H
 
-#define MUF_RE_ICASE		1
-#define MUF_RE_ALL			2
-#define MUF_RE_EXTENDED		4
+#define MUF_RE_ICASE	1
+#define MUF_RE_ALL	2
+#define MUF_RE_EXTENDED	4
 
 void prim_regexp(PRIM_PROTOTYPE);
 void prim_regsub(PRIM_PROTOTYPE);
+void prim_regsplit(PRIM_PROTOTYPE);
+void prim_regsplit_noempty(PRIM_PROTOTYPE);
 
-#define PRIMS_REGEX_FUNCS prim_regexp, prim_regsub
+#define PRIMS_REGEX_FUNCS prim_regexp, prim_regsub, prim_regsplit,	\
+	prim_regsplit_noempty
 
-#define PRIMS_REGEX_NAMES "REGEXP", "REGSUB"
+#define PRIMS_REGEX_NAMES "REGEXP", "REGSUB", "REGSPLIT", "REGSPLIT_NOEMPTY"
 
 #endif				/* _P_REGEX_H */
