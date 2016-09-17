cc7243b370b7bba0ed76162030086788c160563a
wyld-sw <wyld@fuzzball.org>
2016-09-17 13:56:34 -0400

    Aliased &, |, <<, and ^ to BITAND, BTOR, BITSHIFT and BITXOR respectively.

--- include/p_math.h
+++ include/p_math.h
@@ -35,12 +35,12 @@ void prim_notequal(PRIM_PROTOTYPE);
     prim_or, prim_not, prim_lessthan, prim_greathan, prim_equal, prim_lesseq, \
     prim_greateq, prim_random, prim_int, prim_srand, prim_setseed, prim_xor,  \
     prim_getseed, prim_plusplus, prim_minusminus, prim_abs, prim_sign,        \
-    prim_notequal
+    prim_notequal, prim_bitand, prim_bitor, prim_bitxor, prim_bitshift
 
-#define PRIMS_MATH_NAMES  "+",  "-",  "*",  "/",          \
-    "%", "BITOR", "BITXOR", "BITAND", "BITSHIFT", "AND",  \
-    "OR",  "NOT",  "<",  ">",  "=",  "<=",                \
-    ">=", "RANDOM", "INT", "SRAND", "SETSEED", "XOR",     \
-	"GETSEED", "++", "--", "ABS", "SIGN", "!="
+#define PRIMS_MATH_NAMES  "+",  "-",  "*",  "/", "%", "BITOR", \
+    "BITXOR", "BITAND", "BITSHIFT", "AND", "OR",  "NOT",  "<", \
+     ">",  "=",  "<=", ">=", "RANDOM", "INT", "SRAND",         \
+     "SETSEED", "XOR", "GETSEED", "++", "--", "ABS", "SIGN",   \
+     "!=", "&", "|", "^", "<<"
 
 #endif				/* _P_MATH_H */
