c8a8f62abc86bcd98547f9e5ebc3220f3be24188
Shane Synan <digitalcircuit36939@gmail.com>
2017-01-25 22:26:45 -0600

    Switch spaces to tabs, comments to double-slash
    Use tabs for indentation, spaces for alignment.  Enough changes will
    be made here in follow-up commits to warrant cleaning up.
    
    Use // for most multi-line comments, except for function headers.
    Use /// for variable and function headers to follow the Doxygen
    style.
    
    This ignores ssl_log_error() as that will be cleaned
    up in the next commit.
    
    See http://www.stack.nl/~dimitri/doxygen/manual/docblocks.html

--- include/interface_ssl.h
+++ include/interface_ssl.h
@@ -1,39 +1,39 @@
 #ifndef _INTERFACE_SSL_H
 #define _INTERFACE_SSL_H
 
-/* Needed for USE_SSL */
+// Needed for USE_SSL
 #include "config.h"
 
 #ifdef USE_SSL
 
 # ifdef HAVE_OPENSSL
 #  include <openssl/ssl.h>
-/* For nicer error messages */
+// For nicer error messages
 #  include <openssl/err.h>
 # else
 #  include <ssl.h>
-/* TODO: check if other SSL libraries provide this functionality */
+// TODO: check if other SSL libraries provide this functionality
 # endif
 
-/* Backwards-compatibility for valid SSL protocol versions that are not supported. */
+// Backwards-compatibility for valid SSL protocol versions that are not supported.
 # define SSL_UNSUPPORTED_PROTOCOL -2
 
-/* Build a lookup table to convert protocol version string to integer
-
-   Unfortunately OpenSSL lacks an easy way of pragmatically determining what protocols are
-   supported at runtime.  Thus, build a reference table according to compile-time constants.
-   Unavailable protocols are marked with SSL_UNSUPPORTED_PROTOCOL.
-
-   See 'set_protocol_version' in
-   https://github.com/openssl/openssl/blob/master/test/ssltest.c
-
-   To further complicate matters, OpenSSL plans to remove support for older SSL versions over time.
-   Fuzzball needs to provide a fallback to make sure existing setups specifying removed protocol
-   versions will get a useful error message pointing to the problem, instead of entirely failing to
-   compile.
-
-   FB_PROTOCOL_VERSION  Copy OpenSSL's PROTOCOL_VERSION if defined, otherwise SSL_UNSUPPORTED_PROTOCOL
-   FB_PROTOCOL_NAME     Friendly name for error messages, blank if not available */
+// Build a lookup table to convert protocol version string to integer
+//
+// Unfortunately OpenSSL lacks an easy way of pragmatically determining what protocols are
+// supported at runtime.  Thus, build a reference table according to compile-time constants.
+// Unavailable protocols are marked with SSL_UNSUPPORTED_PROTOCOL.
+//
+// See 'set_protocol_version' in
+// https://github.com/openssl/openssl/blob/master/test/ssltest.c
+//
+// To further complicate matters, OpenSSL plans to remove support for older SSL versions over time.
+// Fuzzball needs to provide a fallback to make sure existing setups specifying removed protocol
+// versions will get a useful error message pointing to the problem, instead of entirely failing to
+// compile.
+//
+// FB_PROTOCOL_VERSION  Copy OpenSSL's PROTOCOL_VERSION if defined, otherwise SSL_UNSUPPORTED_PROTOCOL
+// FB_PROTOCOL_NAME     Friendly name for error messages, blank if not available
 #ifdef SSL3_VERSION
 # define FB_SSL3_VERSION SSL3_VERSION
 # define FB_SSL3_NAME "SSLv3 "
@@ -63,32 +63,32 @@
 # define FB_TLS1_2_NAME ""
 #endif
 
-/* List of protocols supported at compile-time, used for error messages */
+// List of protocols supported at compile-time, used for error messages
 #define SSL_KNOWN_PROTOCOLS FB_SSL3_NAME FB_TLS1_NAME FB_TLS1_1_NAME FB_TLS1_2_NAME
 
-/** A single SSL protocol version */
+/// A single SSL protocol version
 struct ssl_protocol_version {
-    const char *name;
-    int version;
+	const char *name;
+	int version;
 };
 
-/** Valid SSL protocol versions */
+/// Valid SSL protocol versions
 static const struct ssl_protocol_version SSL_PROTOCOLS[] = {
-    {"None", 0},
-    {"SSLv3", FB_SSL3_VERSION},
-    {"TLSv1", FB_TLS1_VERSION},
-    {"TLSv1.1", FB_TLS1_1_VERSION},
-    {"TLSv1.2", FB_TLS1_2_VERSION}
+	{"None", 0},
+	{"SSLv3", FB_SSL3_VERSION},
+	{"TLSv1", FB_TLS1_VERSION},
+	{"TLSv1.1", FB_TLS1_1_VERSION},
+	{"TLSv1.2", FB_TLS1_2_VERSION}
 };
 
 static const size_t SSL_PROTOCOLS_SIZE = (sizeof(SSL_PROTOCOLS) / sizeof(SSL_PROTOCOLS[0]));
-/* end of lookup table */
+// End of lookup table
 
-/* SSL protocol management */
+// SSL protocol management
 int ssl_protocol_from_string(const char *);
 int set_ssl_ctx_min_version(SSL_CTX *, const char *);
 
 void ssl_log_error(SSL *, const int);
 
-#endif				/* USE_SSL */
-#endif				/* _INTERFACE_SSL_H */
+#endif                                // USE_SSL
+#endif                                // _INTERFACE_SSL_H
--- src/interface_ssl.c
+++ src/interface_ssl.c
@@ -5,90 +5,95 @@
 #include "interface_ssl.h"
 #include "log.h"
 
-/** Converts an SSL protocol version string to a version number
- *
- *  Inspired by 'set_protocol_version' in
- *  https://github.com/openssl/openssl/blob/master/test/ssltest.c
- *
- *  @param[in] value Name of SSL protocol version
- *  @returns   Version number if found, SSL_UNSUPPORTED_PROTOCOL if unsupported, or -1 if not found
- */
+///
+/// Converts an SSL protocol version string to a version number
+///
+/// Inspired by 'set_protocol_version' in
+/// https://github.com/openssl/openssl/blob/master/test/ssltest.c
+///
+/// @param[in] value Name of SSL protocol version
+/// @returns   Version number if found, SSL_UNSUPPORTED_PROTOCOL if unsupported, or -1 if not found
+///
 int
 ssl_protocol_from_string(const char *value)
 {
-    /* Find the desired version */
-    for (size_t i = 0; i < SSL_PROTOCOLS_SIZE; i++)
-	if (strcmp(SSL_PROTOCOLS[i].name, value) == 0)
-	    return SSL_PROTOCOLS[i].version;
+	// Find the desired version
+	for (size_t i = 0; i < SSL_PROTOCOLS_SIZE; i++)
+		if (strcmp(SSL_PROTOCOLS[i].name, value) == 0)
+			return SSL_PROTOCOLS[i].version;
 
-    /* Not found, return failure */
-    return -1;
+	// Not found, return failure
+	return -1;
 }
 
-/** Sets the minimum SSL protocol version given a version string
- *
- *  If None, no change will be made to the SSL context.  If version string is invalid or unsupported
- *  in this build (see SSL_PROTOCOLS), an error is logged to offer guidance on fixing the problem.
- *
- *  @param[in,out] ssl_ctx     SSL context
- *  @param[in]     min_version Name of minimum required SSL protocol version, or "None"
- *  @returns       1 if successful, otherwise 0
- */
+///
+/// Sets the minimum SSL protocol version given a version string
+///
+/// If None, no change will be made to the SSL context.  If version string is invalid or unsupported
+/// in this build (see SSL_PROTOCOLS), an error is logged to offer guidance on fixing the problem.
+///
+/// @param[in,out] ssl_ctx      SSL context
+/// @param[in]     min_version  Name of minimum required SSL protocol version, or "None"
+/// @returns       1 if successful, otherwise 0
+///
 int
 set_ssl_ctx_min_version(SSL_CTX * ssl_ctx, const char *min_version)
 {
-    int min_version_num = ssl_protocol_from_string(min_version);
-    switch (min_version_num) {
-    case 0:
-	/* None specified, no changes needed */
-	return 1;
-    case -1:
-	log_status("ERROR: Unknown ssl_min_protocol_version '%s'.  Specify one of: %s",
-		   min_version, SSL_KNOWN_PROTOCOLS);
-	fprintf(stderr, "ERROR: Unknown ssl_min_protocol_version '%s'.  Specify one of: %s\n",
-		min_version, SSL_KNOWN_PROTOCOLS);
-	return 0;
-    case SSL_UNSUPPORTED_PROTOCOL:
-	log_status
-		("ERROR: ssl_min_protocol_version '%s' not supported by the SSL library in this build.  Specify one of: %s",
-		 min_version, SSL_KNOWN_PROTOCOLS);
-	fprintf(stderr,
-		"ERROR: ssl_min_protocol_version '%s' not supported by the SSL library in this build.  Specify one of: %s\n",
-		min_version, SSL_KNOWN_PROTOCOLS);
-	return 0;
-    default:
-	log_status("Requiring SSL protocol version '%s' or higher for encrypted connections",
-		   min_version);
+	int min_version_num = ssl_protocol_from_string(min_version);
+	switch (min_version_num) {
+	case 0:
+		// None specified, no changes needed
+		return 1;
+	case -1:
+		log_status("ERROR: Unknown ssl_min_protocol_version '%s'.  Specify one of: %s",
+		           min_version, SSL_KNOWN_PROTOCOLS);
+		fprintf(stderr,
+		        "ERROR: Unknown ssl_min_protocol_version '%s'.  Specify one of: %s\n",
+		        min_version, SSL_KNOWN_PROTOCOLS);
+		return 0;
+	case SSL_UNSUPPORTED_PROTOCOL:
+		log_status("ERROR: ssl_min_protocol_version '%s' not supported by the SSL library "
+		           "in this build.  Specify one of: %s",
+		           min_version, SSL_KNOWN_PROTOCOLS);
+		fprintf(stderr,
+		        "ERROR: ssl_min_protocol_version '%s' not supported by the SSL library "
+		        "in this build.  Specify one of: %s\n",
+		        min_version, SSL_KNOWN_PROTOCOLS);
+		return 0;
+	default:
+		log_status("Requiring SSL protocol version '%s' or higher for encrypted connections",
+		           min_version);
 #if defined(SSL_CTX_set_min_proto_version)
-	/* Added in OpenSSL >= 1.1.0
-	   See: https://www.openssl.org/docs/manmaster/ssl/SSL_CTX_set_min_proto_version.html */
-	return SSL_CTX_set_min_proto_version(ssl_ctx, min_version_num);
+		// Added in OpenSSL >= 1.1.0
+		// See: https://www.openssl.org/docs/manmaster/ssl/SSL_CTX_set_min_proto_version.html
+		return SSL_CTX_set_min_proto_version(ssl_ctx, min_version_num);
 #elif defined(SSL_CTX_set_options)
-	/* Easy way not implemented, manually turn off protocols to match the minimum version.
-	   FB_*_VERSION macros are guaranteed to be defined regardless of OpenSSL version,
-	   either as a real version or SSL_UNSUPPORTED_PROTOCOL, which will never be greater
-	   than min_version_num. */
-	if (min_version_num > FB_SSL3_VERSION)
-	    SSL_CTX_set_options(ssl_ctx, SSL_OP_NO_SSLv3);
-	if (min_version_num > FB_TLS1_VERSION)
-	    SSL_CTX_set_options(ssl_ctx, SSL_OP_NO_TLSv1);
-	if (min_version_num > FB_TLS1_1_VERSION)
-	    SSL_CTX_set_options(ssl_ctx, SSL_OP_NO_TLSv1_1);
-	if (min_version_num > FB_TLS1_2_VERSION)
-	    SSL_CTX_set_options(ssl_ctx, SSL_OP_NO_TLSv1_2);
-	/* No need to add newer versions as OpenSSL >= 1.1.0 supports the much-nicer
-	   SSL_CTX_set_min_proto_version - see above */
-	return 1;
+		// Easy way not implemented, manually turn off protocols to match the minimum version.
+		// FB_*_VERSION macros are guaranteed to be defined regardless of OpenSSL version,
+		// either as a real version or SSL_UNSUPPORTED_PROTOCOL, which will never be greater
+		// than min_version_num.
+		if (min_version_num > FB_SSL3_VERSION)
+			SSL_CTX_set_options(ssl_ctx, SSL_OP_NO_SSLv3);
+		if (min_version_num > FB_TLS1_VERSION)
+			SSL_CTX_set_options(ssl_ctx, SSL_OP_NO_TLSv1);
+		if (min_version_num > FB_TLS1_1_VERSION)
+			SSL_CTX_set_options(ssl_ctx, SSL_OP_NO_TLSv1_1);
+		if (min_version_num > FB_TLS1_2_VERSION)
+			SSL_CTX_set_options(ssl_ctx, SSL_OP_NO_TLSv1_2);
+		// No need to add newer versions as OpenSSL >= 1.1.0 supports the much-nicer
+		// SSL_CTX_set_min_proto_version - see above
+		return 1;
 #else
-	/* A minimum version was requested, but the SSL library doesn't support specifying that.
-	   Error out with some advice. */
-	log_status
-		("ERROR: specifying ssl_min_protocol_version is not supported by the SSL library used in this build.  Set to 'None', or maybe use OpenSSL?");
-	fprintf(stderr,
-		"specifying ssl_min_protocol_version is not supported by the SSL library used in this build.  Set to 'None', or maybe use OpenSSL?\n");
-	return 0;
+		// A minimum version was requested, but the SSL library doesn't support specifying that.
+		// Error out with some advice.
+		log_status("ERROR: specifying ssl_min_protocol_version is not supported by the SSL "
+		           "library used in this build.  Set to 'None', or maybe use OpenSSL?");
+		fprintf(stderr,
+		        "specifying ssl_min_protocol_version is not supported by the SSL "
+		        "library used in this build.  Set to 'None', or maybe use OpenSSL?\n");
+		return 0;
 #endif
-    }
+	}
 }
 
 /** Records the last SSL error to the log
@@ -140,4 +145,4 @@ ssl_log_error(SSL * ssl, const int ret_value)
     }
 }
 
-#endif				/* USE_SSL */
+#endif				// USE_SSL
