7682729a95b33d6820f17c2e9f804fbdca51e070
wyld-sw <wyld@fuzzball.org>
2017-09-10 10:01:35 -0400

    Removed regressions db in favor of external testing.

--- dbs/regressions/muf/6.m
+++
@@ -1,7 +0,0 @@
-: main[ str:Args -- ]
-    99999999  abs 99999999 = not if "Positive abs failed." abort then
-    -99999999 abs 99999999 = not if "Negative abs failed!" abort then
-    99999999  sign  1 = not if "Positive sign failed." abort then
-    -99999999 sign -1 = not if "Negative sign failed." abort then
-    0 sign 0 = not if "Zero sign failed." abort then
-;
