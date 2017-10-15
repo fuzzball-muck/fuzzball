535ffbb9dc1e96d3dd8f7dce519f9a3bddc37af6
wyld-sw <wyld@fuzzball.org>
2017-10-15 11:21:19 -0400

    Revert "Relocated longest_match and partial_exits to function scope."
    This reverts commit bc98723afab1e3d2082709dbfcdcb5793c87b529.

--- src/match.c
+++ src/match.c
@@ -27,8 +27,10 @@ init_match(int descr, dbref player, const char *name, int type, struct match_dat
     md->match_name = name;
     md->check_keys = 0;
     md->preferred_type = type;
+    md->longest_match = 0;
     md->match_level = 0;
     md->block_equals = 0;
+    md->partial_exits = (TYPE_EXIT == type);
 }
 
 void
@@ -273,7 +275,6 @@ match_exits(dbref obj, struct match_data *md)
     dbref exit, absolute, first;
     const char *exitname, *p;
     int exitprog, lev, partial;
-    size_t longest_match = 0;
 
     first = EXITS(obj);
 
@@ -300,7 +301,7 @@ match_exits(dbref obj, struct match_data *md)
 		    || Typeof((DBFETCH(exit)->sp.exit.dest)[i]) == TYPE_PROGRAM)
 		    exitprog = 1;
 	}
-	if (tp_enable_prefix && exitprog && md->preferred_type == TYPE_EXIT &&
+	if (tp_enable_prefix && exitprog && md->partial_exits &&
 	    (FLAGS(exit) & XFORCIBLE) && FLAGS(OWNER(exit)) & WIZARD) {
 	    partial = 1;
 	} else {
@@ -329,13 +330,13 @@ match_exits(dbref obj, struct match_data *md)
 		if (*exitname == '\0' || *exitname == EXIT_DELIMITER) {
 		    /* we got a match on this alias */
 		    if (lev >= md->match_level) {
-			if (strlen(md->match_name) - strlen(p) > longest_match) {
+			if (strlen(md->match_name) - strlen(p) > (size_t) md->longest_match) {
 			    if (lev > md->match_level) {
 				md->match_level = lev;
 				md->block_equals = 0;
 			    }
 			    md->exact_match = exit;
-			    longest_match = strlen(md->match_name) - strlen(p);
+			    md->longest_match = strlen(md->match_name) - strlen(p);
 			    if ((*p == ' ') || (partial && notnull)) {
 				strcpyn(match_args, sizeof(match_args),
 					(partial && notnull) ? p : (p + 1));
@@ -354,7 +355,7 @@ match_exits(dbref obj, struct match_data *md)
 					(char *) md->match_name);
 			    }
 			} else if ((strlen(md->match_name) - strlen(p) ==
-				    longest_match) && !((lev == md->match_level)
+				    (size_t) md->longest_match) && !((lev == md->match_level)
 								     && (md->block_equals))) {
 			    if (lev > md->match_level) {
 				md->exact_match = exit;
