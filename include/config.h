1b7f543d6f15c36a2de46ea1e040f5b796b81282
Cat <deepbluemistake@gmail.com>
2019-02-27 06:58:39 -0500

    Change version number

--- include/config.h
+++ include/config.h
@@ -15,7 +15,7 @@
 
 #include "autoconf.h"
 
-#define VERSION "Muck2.2fb7.00b1"
+#define VERSION "Muck2.2fb7.00b1.20190227a"
 
 /************************************************************************
    Administrative Options 
