c7da25bb9dc88673c8965783baee7f993e82d82f
Cat <deepbluemistake@gmail.com>
2019-03-04 16:20:59 -0500

    Update config.h

--- include/config.h
+++ include/config.h
@@ -15,7 +15,7 @@
 
 #include "autoconf.h"
 
-#define VERSION "Muck2.2fb7.00b1.20190304a"
+#define VERSION "Muck2.2fb7.00b1+master"
 
 /************************************************************************
    Administrative Options 
