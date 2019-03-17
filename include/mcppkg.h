b804e20f235aa53e591ed7452ce52184dee10812
Cat <deepbluemistake@gmail.com>
2019-03-17 18:10:37 -0400

    Update mcppkg.h

--- include/mcppkg.h
+++ include/mcppkg.h
@@ -4,6 +4,8 @@
 #ifndef _MCPPKG_H
 #define _MCPPKG_H
 
+#include "mcp.h"
+
 void mcppkg_simpleedit(McpFrame * mfr, McpMesg * msg, McpVer ver, void *context);
 void mcppkg_languages(McpFrame * mfr, McpMesg * msg, McpVer ver, void *context);
 void mcppkg_help_request(McpFrame * mfr, McpMesg * msg, McpVer ver, void *context);
