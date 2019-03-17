5358f0300f4374c151f6ff55580821ced6ff8115
Cat <deepbluemistake@gmail.com>
2019-03-17 13:58:36 -0400

    Fix includes in .c files (array.c)
    fixes in .h files will come later
    
    TODO: put config.h in with all the other local includes, don't emphasise it as special
    but, it's still special, so it needs to be made less special in the future
    
    honestly, this'd be easier after getting rid of autoconf, but I'm doing it now since I know how to do this now. :P
    
    also, strings.h wasn't included here since it'd break windows builds

--- src/array.c
+++ src/array.c
@@ -6,6 +6,13 @@
  * This file is part of Fuzzball MUCK.  Please see LICENSE.md for details.
  */
 
+#include <assert.h>
+#include <float.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "config.h"
 
 #include "array.h"
