895edd31dc87169ec619b5b1b12d0f84867193ab
Cat <deepbluemistake@gmail.com>
2019-03-17 14:27:42 -0400

    Update fbmath.c

--- src/fbmath.c
+++ src/fbmath.c
@@ -1,4 +1,9 @@
-#include "config.h"
+#include <limits.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 #include "fbmath.h"
 #include "inst.h"
