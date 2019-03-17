9746c0bbf1d883b04a838028a570913ce22f438e
Cat <deepbluemistake@gmail.com>
2019-03-17 18:32:22 -0400

    Update tunelist.h

--- include/tunelist.h
+++ include/tunelist.h
@@ -8,6 +8,7 @@
 #include "defaults.h"
 #include "fbtime.h"
 #include "game.h"
+#include "tune.h"
 
 /* Specify the same default values in the pointer and in the lists of tune_*_entry.
    Default values here will be used if the tunable isn't found, default values in the lists of tune_*_entry
