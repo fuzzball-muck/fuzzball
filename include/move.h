1f4a7510f12c2d79b1df00189f663ba2fa346a3d
Cat <deepbluemistake@gmail.com>
2019-03-17 10:59:14 -0400

    Update move.h
    move.h uses dbref, defined in config.h
    Should we move some of this stuff out of config.h and into something like defines.h or fuzzball.h?  I feel like configuration and non-configurable typedefs shouldn't be in the same file.

--- include/move.h
+++ include/move.h
@@ -1,6 +1,8 @@
 #ifndef _MOVE_H
 #define _MOVE_H
 
+#include "config.h"
+
 void enter_room(int descr, dbref player, dbref loc, dbref exit);
 void moveto(dbref what, dbref where);
 void send_contents(int descr, dbref loc, dbref dest);
