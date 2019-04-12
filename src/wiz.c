17c5e893d2c1a97b3f2944b211d8dbb002f98c17
Cat <deepbluemistake@gmail.com>
2019-04-12 05:28:16 -0400

    Show microseconds of user and system time
    It turns out fuzzball is just incredibly efficient and doesn't easily use whole seconds of CPU time. :3

--- src/wiz.c
+++ src/wiz.c
@@ -1061,8 +1061,8 @@ do_usage(dbref player)
     notifyf(player, "Swapped out of main memory %d times.", usage.ru_nswap);
     notifyf(player, "Voluntarily context switched %d times.", usage.ru_nvcsw);
     notifyf(player, "Involuntarily context switched %d times.", usage.ru_nivcsw);
-    notifyf(player, "User time used: %ld sec.", usage.ru_utime.tv_sec);
-    notifyf(player, "System time used: %ld sec.", usage.ru_stime.tv_sec);
+    notifyf(player, "User time used: %ld sec and %ld usec.", usage.ru_utime.tv_sec, usage.ru_utime.tv_usec);
+    notifyf(player, "System time used: %ld sec and %ld usec.", usage.ru_stime.tv_sec, usage.ru_stime.tv_usec);
     notifyf(player, "Pagesize for this machine: %ld", psize);
     notifyf(player, "Maximum resident memory: %ldk",
             (long) (usage.ru_maxrss * (psize / 1024)));
