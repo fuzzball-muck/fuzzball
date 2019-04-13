18b623ce0f8f6812c33891a4886a163da4ce160d
tanabi <cheetah@tanabi.org>
2019-04-13 06:35:13 -0400

    Documentation for fbsignal (#414)
    * Added fbsignal documentation
    
    * Fixed a stray tab

--- include/fbsignal.h
+++ include/fbsignal.h
@@ -1,10 +1,30 @@
+/** @file fbsignal.h
+ *
+ * Header for the declaration of Fuzzball's signal handling system.
+ *
+ * This file is part of Fuzzball MUCK.  Please see LICENSE.md for details.
+ */
+
 #ifndef FBSIGNAL_H
 #define FBSIGNAL_H
 
+/**
+ * Set signals for the forked dump processes
+ *
+ * These signals are not necessarily the same as the signals for the main
+ * program, but are tailored towards the needs of the dump process.
+ */
 void set_dumper_signals(void);
+
+/**
+ * This does the initial signal setup.
+ */
 void set_signals(void);
 
 #ifdef HAVE_PSELECT
+/**
+ * @var the signal mask for pselect - this is set by set_signals
+ */
 extern sigset_t pselect_signal_mask;
 #endif
 
--- src/fbsignal.c
+++ src/fbsignal.c
@@ -1,3 +1,10 @@
+/** @file fbsignal.c
+ *
+ * Definitions for Fuzzball's signal handling system.
+ *
+ * This file is part of Fuzzball MUCK.  Please see LICENSE.md for details.
+ */
+
 #include <errno.h>
 #include <signal.h>
 #include <stdio.h>
@@ -17,6 +24,13 @@
 
 /*
  * Function prototypes
+ *
+ * @TODO remove set_signals(void) because it is declared in fbsignal.h
+ *       I believe the rest of these are needed because they are signal
+ *       callbacks?  Normally you don't need prototypes for local functions.
+ *       That's probably why these aren't static as well.
+ *
+ * See definitions for doc blocks.
  */
 void set_signals(void);
 void bailout(int);
@@ -32,22 +46,36 @@ void sig_reap(int i);
 #endif
 
 #ifdef HAVE_PSELECT
+/**
+ * @private
+ * @var boolean true if we have set the pselect_signal_mask, starts as false.
+ */
 static int set_pselect_signal_mask = 0;
+
+/**
+ * @var the signal mask for pselect - this is set by set_signals
+ */
 sigset_t pselect_signal_mask;
 #endif
 
+/**
+ * Set signals for the forked dump processes
+ *
+ * These signals are not necessarily the same as the signals for the main
+ * program, but are tailored towards the needs of the dump process.
+ */
 void
 set_dumper_signals(void)
 {
-    signal(SIGPIPE, SIG_IGN);	/* Ignore Blocked Pipe */
-    signal(SIGHUP, SIG_IGN);	/* Ignore Terminal Hangup */
-    signal(SIGCHLD, SIG_IGN);	/* Ignore Child termination */
-    signal(SIGFPE, SIG_IGN);	/* Ignore FP exceptions */
-    signal(SIGUSR1, SIG_IGN);	/* Ignore SIGUSR1 */
-    signal(SIGUSR2, SIG_IGN);	/* Ignore SIGUSR2 */
-    signal(SIGINT, SIG_DFL);	/* Take Interrupt signal and die! */
-    signal(SIGTERM, SIG_DFL);	/* Take Terminate signal and die! */
-    signal(SIGSEGV, SIG_DFL);	/* Take Segfault and die! */
+    signal(SIGPIPE, SIG_IGN);   /* Ignore Blocked Pipe */
+    signal(SIGHUP, SIG_IGN);    /* Ignore Terminal Hangup */
+    signal(SIGCHLD, SIG_IGN);   /* Ignore Child termination */
+    signal(SIGFPE, SIG_IGN);    /* Ignore FP exceptions */
+    signal(SIGUSR1, SIG_IGN);   /* Ignore SIGUSR1 */
+    signal(SIGUSR2, SIG_IGN);   /* Ignore SIGUSR2 */
+    signal(SIGINT, SIG_DFL);    /* Take Interrupt signal and die! */
+    signal(SIGTERM, SIG_DFL);   /* Take Terminate signal and die! */
+    signal(SIGSEGV, SIG_DFL);   /* Take Segfault and die! */
 #ifdef SIGTRAP
     signal(SIGTRAP, SIG_DFL);
 #endif
@@ -64,31 +92,33 @@ set_dumper_signals(void)
     signal(SIGSYS, SIG_DFL);
 #endif
 #ifdef SIGXCPU
-    signal(SIGXCPU, SIG_IGN);	/* CPU usage limit exceeded */
+    signal(SIGXCPU, SIG_IGN);   /* CPU usage limit exceeded */
 #endif
 #ifdef SIGXFSZ
-    signal(SIGXFSZ, SIG_IGN);	/* Exceeded file size limit */
+    signal(SIGXFSZ, SIG_IGN);   /* Exceeded file size limit */
 #endif
 #ifdef SIGVTALRM
     signal(SIGVTALRM, SIG_DFL);
 #endif
 }
 
-
-/*
- * set_signals()
- * set_sigs_intern(bail)
- *
- * Traps a bunch of signals and reroutes them to various
- * handlers. Mostly bailout.
- *
- * If called from bailout, then reset all to default.
- *
- * Called from main() and bailout()
- */
+/* Helper defines for set_sigs_intern */
 #define SET_BAIL (bail ? SIG_DFL : bailout)
 #define SET_IGN  (bail ? SIG_DFL : SIG_IGN)
 
+/**
+ * Internal signal settings
+ *
+ * Traps a bunch of signals and reroutes them to various handlers. Mostly
+ * 'bailout'.  @see bailout
+ *
+ * If called from bailout, then reset all to default.  'bail' is true
+ * when called from bailout.
+ *
+ * Called from main() and bailout()
+ *
+ * @param bail boolean true if called from 'bailout' function.
+ */
 static void
 set_sigs_intern(int bail)
 {
@@ -107,7 +137,6 @@ set_sigs_intern(int bail)
     signal(SIGTERM, SET_BAIL);
 
     /* catch these because we might as well */
-/*  signal(SIGQUIT, SET_BAIL);  */
 #ifdef SIGTRAP
     signal(SIGTRAP, SET_IGN);
 #endif
@@ -124,9 +153,6 @@ set_sigs_intern(int bail)
     signal(SIGSYS, SET_BAIL);
 #endif
     signal(SIGFPE, SET_BAIL);
-#if 0
-    signal(SIGSEGV, SET_BAIL);
-#endif
     signal(SIGTERM, bail ? SET_BAIL : sig_shutdown);
 #ifdef SIGXCPU
     signal(SIGXCPU, SET_BAIL);
@@ -138,7 +164,10 @@ set_sigs_intern(int bail)
     signal(SIGVTALRM, SET_BAIL);
 #endif
 #ifdef SIGEMERG
-    /* Clean shutdown signal (may be used with UPS software when power goes down...) */
+    /*
+     * Clean shutdown signal (may be used with UPS software when power goes
+     * down...)
+     */
     signal(SIGUSR2, sig_emerg);
 #else
     signal(SIGUSR2, SET_BAIL);
@@ -148,13 +177,17 @@ set_sigs_intern(int bail)
     signal(SIGUSR1, bail ? SIG_DFL : sig_dump_status);
 
 #ifdef HAVE_PSELECT
+    /* This is only run once */
     if (!set_pselect_signal_mask) {
         sigset_t signals_to_block;
 
         sigemptyset(&signals_to_block);
-        /* block signals we expect during normal operation from that it
-           should be okay to wait for the current MUCK event to finish
-           before processing */
+
+        /*
+         * block signals we expect during normal operation from that it
+         * should be okay to wait for the current MUCK event to finish
+         * before processing
+         */
         sigaddset(&signals_to_block, SIGCHLD);
         sigaddset(&signals_to_block, SIGUSR1);
 
@@ -165,6 +198,15 @@ set_sigs_intern(int bail)
 #endif
 }
 
+/**
+ * This does the initial signal setup.
+ *
+ * @TODO We could just rename set_sigs_intern to set_signals and take
+ *       the boolean parameter rather than have this dinky little method.
+ *       set_signals is only called in interface.c so making it pass
+ *       0/false is no big deal.  If you change it, remember to update
+ *       the header documentation.
+ */
 void
 set_signals(void)
 {
@@ -175,15 +217,29 @@ set_signals(void)
  * Signal handlers
  */
 
-/*
- * BAIL!
+/**
+ * Handle a situation where the MUCK must exit due to a signal.
+ *
+ * This results in the MUCK exiting with code 7.
+ *
+ * @param sig the signal number called.
  */
 void
 bailout(int sig)
 {
     char message[1024];
 
-    /* turn off signals */
+    /*
+     * Turn off signals
+     *
+     * @TODO Do we really need to do this?  This complicates set_sigs_intern
+     *       and I don't really get why we need to do it unless the signal
+     *       settings are interfering with exit(...) on some OSes?  I would
+     *       suggest commenting this out and throwing some signals at the
+     *       MUCK, see if it comes down.  If we don't need to do this, then
+     *       we can get rid of the parameter and all the weird 'bail' if
+     *       statements.
+     */
     set_sigs_intern(1);
 
     snprintf(message, sizeof(message), "BAILOUT: caught signal %d", sig);
@@ -193,8 +249,10 @@ bailout(int sig)
     exit(7);
 }
 
-/*
+/**
  * Spew WHO to file
+ *
+ * @param i the signal number (not used)
  */
 void
 sig_dump_status(int i)
@@ -203,17 +261,34 @@ sig_dump_status(int i)
 }
 
 #ifdef SIGEMERG
+/**
+ * Handle SIGEMERG
+ *
+ * This will shut down the MUCK and send a wall message to the MUCK.
+ * Database will be dumped as well.
+ *
+ * @param i signal number (ignored)
+ */
 void
 sig_emerg(int i)
 {
+    /*
+     * @TODO This should probably log_status as well ?
+     */
     wall_and_flush
-	    ("\nEmergency signal received ! (power failure ?)\nThe database will be saved.\n");
+        ("\nEmergency signal received ! (power failure ?)\nThe database will be saved.\n");
     dump_database();
     shutdown_flag = 1;
     restart_flag = 0;
 }
 #endif
 
+/**
+ * Send status to wizards and log file
+ *
+ * @private
+ * @param s the message to send
+ */
 static void
 wall_status(char *s)
 {
@@ -224,13 +299,16 @@ wall_status(char *s)
     wall_wizards(buf);
 }
 
-/*
- * Reload configuration.
+/**
+ * Reload SSL configuration based on signal
+ *
+ * @param i the signal number (ignored)
  */
 void
 sig_reconfigure(int i)
 {
     wall_status("Configuration reload requested remotely.");
+
 #ifdef USE_SSL
     if (reconfigure_ssl()) {
         wall_status("Certificate reload was successful.");
@@ -238,11 +316,14 @@ sig_reconfigure(int i)
         wall_status("Certificate reload failed!");
     }
 #endif
+
     wall_status("Configuration reload complete.");
 }
 
-/*
+/**
  * Gracefully shut the server down.
+ *
+ * @param i the signal number (ignored)
  */
 void
 sig_shutdown(int i)
@@ -260,29 +341,36 @@ sig_shutdown(int i)
 void
 sig_reap(int i)
 {
-    /* If DISKBASE is not defined, then there are two types of
+    /*
+     * If DISKBASE is not defined, then there are two types of
      * children that can die.  First is the nameservice resolver.
      * Second is the database dumper.  If resolver exits, we should
      * note it in the log -- at least give the admin the option of
-     * knowing about it, and dealing with it as necessary. */
-
-    /* The fix for SSL connections getting closed when databases were
+     * knowing about it, and dealing with it as necessary.
+     *
+     * The fix for SSL connections getting closed when databases were
      * saved with DISKBASE disabled required closing all sockets 
      * when the server fork()ed.  This made it impossible for that
      * process to spit out the "save done" message.  However, because
      * that process dies as soon as it finishes dumping the database,
      * can detect that the child died, and broadcast the "save done"
-     * message anyway. */
+     * message anyway.
+     */
 
     int status = 0;
     int reapedpid = 0;
     int need_to_spawn_resolver = 0;
-    /* look for children to reap in a loop in case a resolver and dumper
-       process die at almost the same time. */
+
+    /*
+     * look for children to reap in a loop in case a resolver and dumper
+     * process die at almost the same time.
+     */
     do {
         reapedpid = waitpid(-1, &status, WNOHANG);
+
         if (reapedpid == global_resolver_pid) {
             log_status("resolver exited with status %d", status);
+
             if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
                 /* If the resolver exited with an error, respawn it. */
                 need_to_spawn_resolver = 1;
@@ -290,6 +378,7 @@ sig_reap(int i)
                 /* If the resolver exited due to a signal, respawn it. */
                 need_to_spawn_resolver = 1;
             }
+
 #ifndef DISKBASE
         } else if (reapedpid == global_dumper_pid) {
             int warnflag = 0;
@@ -328,38 +417,70 @@ sig_reap(int i)
         }
     } while (reapedpid != -1 && reapedpid != 0);
 
-    /* spawn the resolver after the loop so if it exits immediately,
-       we still manage to exit this signal handler. */
+    /*
+     * spawn the resolver after the loop so if it exits immediately,
+     * we still manage to exit this signal handler.
+     */
     if (need_to_spawn_resolver) {
         spawn_resolver();
     }
 }
 #endif
 
-#else				/* WIN32 */
+#else /* WIN32 */
+
+/*
+ * In the case of windows, we actually ignore most of this stuff.
+ *
+ * I don't know how doxygen will handle these "duplicates" so I am not
+ * using docblock tags. (tanabi)
+ */
 
 #ifdef SPAWN_HOST_RESOLVER
+/*
+ * FOR WINDOWS: reap does nothing.
+ *
+ * @param i the signal number (ignored)
+ */
 void
 sig_reap(int i)
 {
 }
 #endif
 
+/*
+ * FOR WINDOWS: shutdown does nothing.
+ *
+ * @param i the signal number (ignored)
+ */
 void
 sig_shutdown(int i)
 {
 }
 
+/*
+ * FOR WINDOWS: set signal internal does nothing
+ *
+ * @param bail (ignored)
+ */
 void
 set_sigs_intern(int bail)
 {
 }
 
+/*
+ * FOR WINDOWS: set signal does nothing
+ */
 void
 set_signals()
 {
 }
 
+/*
+ * FOR WINDOWS: bailout is simplified
+ *
+ * @param sig the signal number
+ */
 void
 bailout(int sig)
 {
@@ -369,30 +490,44 @@ bailout(int sig)
     exit(7);
 }
 
-
+/**
+ * Windows Console Control Handler
+ *
+ * This handles different "controls", such as CTRL-C, close events,
+ * shutdown, etc.  Its basically an event handler for the console
+ * window.
+ *
+ * @param mesg the event type
+ * @return boolean true if event was processed/handled, false otherwise.
+ */
 BOOL WINAPI
 HandleConsole(DWORD mesg)
 {
     switch (mesg) {
-    case CTRL_C_EVENT:
-    case CTRL_BREAK_EVENT:
-	break;
+        case CTRL_C_EVENT:
+        case CTRL_BREAK_EVENT:
+            break;
 
-    case CTRL_CLOSE_EVENT:
-    case CTRL_LOGOFF_EVENT:
-    case CTRL_SHUTDOWN_EVENT:
-	shutdown_flag = 1;
-	restart_flag = 0;
-	log_status("SHUTDOWN: via SIGNAL");
-	break;
+        case CTRL_CLOSE_EVENT:
+        case CTRL_LOGOFF_EVENT:
+        case CTRL_SHUTDOWN_EVENT:
+            shutdown_flag = 1;
+            restart_flag = 0;
+            log_status("SHUTDOWN: via SIGNAL");
+            break;
 
-    default:
-	return 0;
+        default:
+            return 0;
     }
 
     return 1;
 }
 
+/**
+ * Windows console setup
+ *
+ * Does windows-specific console setup items.
+ */
 void
 set_console()
 {
@@ -400,4 +535,4 @@ set_console()
     SetConsoleTitle(VERSION);
 }
 
-#endif				/* WIN32 */
+#endif /* WIN32 */
