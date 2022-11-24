fuzzball-muck/fuzzball

commit e35d6543ab55ebb4b2fba33de37b59b74665bd95

Replaced several calls to snprintf/notify[_nolisten] with a single call to notifyf[_nolisten]

src/interp.c
@@ -1178,7 +1178,6 @@ interp_loop(dbref player, dbref program, struct frame *fr, int rettyp)
 			}
 			if (breakflag) {
 				char *m;
-				char buf[BUFFER_LEN];
 
 				if (fr->brkpt.dosyspop) {
 					program = sys[--stop].progref;
@@ -1203,8 +1202,7 @@ interp_loop(dbref player, dbref program, struct frame *fr, int rettyp)
 					list_proglines(player, program, fr, pc->line, 0);
 				} else {
 					m = show_line_prims(fr, program, pc, 15, 1);
-					snprintf(buf, sizeof(buf), "     %s", m);
-					notify_nolisten(player, buf, 1);
+					notifyf_nolisten(player, "     %s", m);
 				}
 				calc_profile_timing(program,fr);
 				return NULL;
@@ -1809,9 +1807,8 @@ interp_err(dbref player, dbref program, struct inst *pc,
 	}
 	notify_nolisten(player, buf, 1);
 
-	snprintf(buf, sizeof(buf), "\033[1m%s(#%d), line %d; %s: %s\033[0m",
+	notifyf_nolisten(player, "\033[1m%s(#%d), line %d; %s: %s\033[0m",
 			 NAME(program), program, pc ? pc->line : -1, msg1, msg2);
-	notify_nolisten(player, buf, 1);
 
 	lt = time(NULL);
 	format_time(tbuf, 32, "%c", MUCK_LOCALTIME(lt));

include/db.h
@@ -247,18 +247,14 @@ typedef long object_flag_type;
 #define NOGUEST(_cmd,x) \
 if(ISGUEST(x)) \
 {   \
-    char tmpstr[BUFFER_LEN]; \
     log_status("Guest %s(#%d) failed attempt to %s.\n",NAME(x),x,_cmd); \
-    snprintf(tmpstr, sizeof(tmpstr), "Guests are not allowed to %s.\r", _cmd); \
-    notify_nolisten(x,tmpstr,1); \
+    notifyf_nolisten(x, "Guests are not allowed to %s.\r", _cmd); \
     return; \
 }
 #define NOFORCE(_cmd,fl,x) \
 if(fl) \
 {   \
-    char tmpstr[BUFFER_LEN]; \
-    snprintf(tmpstr, sizeof(tmpstr), "You can't use %s from a @force or {force}.\r", _cmd); \
-    notify_nolisten(x,tmpstr,1); \
+    notifyf_nolisten(x, "You can't use %s from a @force or {force}.\r", _cmd); \
     return; \
 }
 
@@ -310,36 +306,28 @@ if(fl) \
 #define BUILDERONLY(_cmd,x) \
 if(!Builder(x)) \
 {   \
-    char tmpstr[BUFFER_LEN]; \
-    snprintf(tmpstr, sizeof(tmpstr), "Only builders are allowed to %s.\r", _cmd); \
-    notify_nolisten(x,tmpstr,1); \
+    notifyf_nolisten(x, "Only builders are allowed to %s.\r", _cmd); \
     return; \
 }
 
 #define MUCKERONLY(_cmd,x) \
 if(!Mucker(x)) \
 {   \
-    char tmpstr[BUFFER_LEN]; \
-    snprintf(tmpstr, sizeof(tmpstr), "Only programmers are allowed to %s.\r", _cmd); \
-    notify_nolisten(x,tmpstr,1); \
+    notifyf_nolisten(x, "Only programmers are allowed to %s.\r", _cmd); \
     return; \
 }
 
 #define PLAYERONLY(_cmd,x) \
 if(Typeof(x) != TYPE_PLAYER) \
 {   \
-    char tmpstr[BUFFER_LEN]; \
-    snprintf(tmpstr, sizeof(tmpstr), "Only players are allowed to %s.\r", _cmd); \
-    notify_nolisten(x,tmpstr,1); \
+    notifyf_nolisten(x, "Only players are allowed to %s.\r", _cmd); \
     return; \
 }
 
 #define WIZARDONLY(_cmd,x) \
 if(!Wizard(OWNER(x))) \
 {   \
-    char tmpstr[BUFFER_LEN]; \
-    snprintf(tmpstr, sizeof(tmpstr), "You are not allowed to %s.\r", _cmd); \
-    notify_nolisten(x,tmpstr,1); \
+    notifyf_nolisten(x, "You are not allowed to %s.\r", _cmd); \
     return; \
 }
 
@@ -349,9 +337,7 @@ if(!Wizard(OWNER(x))) \
  #define GODONLY(_cmd,x) \
 if(!God(x)) \
 {   \
-    char tmpstr[BUFFER_LEN]; \
-    snprintf(tmpstr, sizeof(tmpstr), "You are not allowed to %s.\r", _cmd); \
-    notify_nolisten(x,tmpstr,1); \
+    notifyf_nolisten(x, "You are not allowed to %s.\r", _cmd); \
     return; \
 }
 #endif

include/externs.h
@@ -118,16 +118,18 @@ extern void spit_file_segment(dbref player, const char *filename, const char *se
 extern void do_armageddon(dbref player, const char *msg);
 extern void dump_status(void);
 extern void flush_user_output(dbref player);
+extern short global_dumpdone;
 #ifndef DISKBASE
 extern pid_t global_dumper_pid;
 #endif
-extern short global_dumpdone;
 extern pid_t global_resolver_pid;
 extern long max_open_files(void);
 extern int notify(dbref player, const char *msg);
 extern int notify_from(dbref from, dbref player, const char *msg);
 extern int notify_from_echo(dbref from, dbref player, const char *msg, int isprivate);
 extern int notify_nolisten(dbref player, const char *msg, int isprivate);
+extern void notifyf(dbref player, char *format, ...);
+extern void notifyf_nolisten(dbref player, char *format, ...);
 extern int pdescrsecure(int c);
 extern int pset_user(int c, dbref who);
 extern long sel_prof_idle_sec;
@@ -159,7 +161,6 @@ extern void log_program_text(struct line *first, dbref player, dbref i);
 extern void log_sanity(char *format, ...);
 extern void log_status(char *format, ...);
 extern void log_user(dbref player, dbref program, char *logmessage);
-extern void notify_fmt(dbref player, char *format, ...);
 
 /* look.c */
 extern void do_contents(int descr, dbref player, const char *name, const char *flags);

include/msgparse.h
@@ -62,9 +62,9 @@ int free_mfuncs(int downto);
 #define MFUNARGS int descr, dbref player, dbref what, dbref perms, int argc, \
                 argv_typ argv, char *buf, int buflen, int mesgtyp
 
-#define CHECKRETURN(vari,funam,num) if (!vari) { snprintf(buf, BUFFER_LEN, "%s %c%s%c (%s)", get_mvar("how"), MFUN_LEADCHAR, funam, MFUN_ARGEND, num);  notify_nolisten(player, buf, 1);  return NULL; }
+#define CHECKRETURN(vari,funam,num) if (!vari) { notifyf_nolisten(player, "[]%s %c%s%c (%s)", get_mvar("how"), MFUN_LEADCHAR, funam, MFUN_ARGEND, num); return NULL; }
 
-#define ABORT_MPI(funam,mesg) { snprintf(buf, BUFFER_LEN, "%s %c%s%c: %s", get_mvar("how"), MFUN_LEADCHAR, funam, MFUN_ARGEND, mesg);  notify_nolisten(player, buf, 1);  return NULL; }
+#define ABORT_MPI(funam,mesg) { notifyf_nolisten(player, "[]%s %c%s%c: %s", get_mvar("how"), MFUN_LEADCHAR, funam, MFUN_ARGEND, mesg); return NULL; }
 
 typedef char **argv_typ;
 

src/boolexp.c
@@ -188,7 +188,6 @@ parse_boolexp_F(int descr, const char **parsebuf, dbref player, int dbloadp)
 	char *p;
 	struct match_data md;
 	char buf[BUFFER_LEN];
-	char msg[BUFFER_LEN];
 
 	skip_whitespace(parsebuf);
 	switch (**parsebuf) {
@@ -252,13 +251,11 @@ parse_boolexp_F(int descr, const char **parsebuf, dbref player, int dbloadp)
 			b->thing = match_result(&md);
 
 			if (b->thing == NOTHING) {
-				snprintf(msg, sizeof(msg), "I don't see %s here.", buf);
-				notify(player, msg);
+				notifyf(player, "I don't see %s here.", buf);
 				free_boolnode(b);
 				return TRUE_BOOLEXP;
 			} else if (b->thing == AMBIGUOUS) {
-				snprintf(msg, sizeof(msg), "I don't know which %s you mean!", buf);
-				notify(player, msg);
+				notifyf(player, "I don't know which %s you mean!", buf);
 				free_boolnode(b);
 				return TRUE_BOOLEXP;
 			} else {

src/compile.c
@@ -1388,9 +1388,7 @@ do_compile(int descr, dbref player_in, dbref program_in, int force_err_display)
 		} while (optcnt > 0 && --maxpasses > 0);
 
 		if (force_err_display && optimcount > 0) {
-			char buf[BUFFER_LEN];
-			snprintf(buf, sizeof(buf), "Program optimized by %d instructions in %d passes.", optimcount, passcount);
-			notify_nolisten(cstat.player, buf, 1);
+			notifyf_nolisten(cstat.player, "Program optimized by %d instructions in %d passes.", optimcount, passcount);
 		}
 	}
 

src/create.c
@@ -27,7 +27,6 @@ static dbref
 parse_linkable_dest(int descr, dbref player, dbref exit, const char *dest_name)
 {
 	dbref dobj;					/* destination room/player/thing/link */
-	static char buf[BUFFER_LEN];
 	struct match_data md;
 
 	init_match(descr, player, dest_name, NOTYPE, &md);
@@ -37,16 +36,14 @@ parse_linkable_dest(int descr, dbref player, dbref exit, const char *dest_name)
 	match_nil(&md);
 
 	if ((dobj = match_result(&md)) == NOTHING || dobj == AMBIGUOUS) {
-		snprintf(buf, sizeof(buf), "I couldn't find '%s'.", dest_name);
-		notify(player, buf);
+		notifyf(player, "I couldn't find '%s'.", dest_name);
 		return NOTHING;
 
 	}
 
 	if (!tp_teleport_to_player && Typeof(dobj) == TYPE_PLAYER) {
-		snprintf(buf, sizeof(buf), "You can't link to players.  Destination %s ignored.",
+		notifyf(player, "You can't link to players.  Destination %s ignored.",
 				unparse_object(player, dobj));
-		notify(player, buf);
 		return NOTHING;
 	}
 
@@ -55,8 +52,7 @@ parse_linkable_dest(int descr, dbref player, dbref exit, const char *dest_name)
 		return NOTHING;
 	}
 	if (!can_link_to(player, Typeof(exit), dobj)) {
-		snprintf(buf, sizeof(buf), "You can't link to %s.", unparse_object(player, dobj));
-		notify(player, buf);
+		notifyf(player, "You can't link to %s.", unparse_object(player, dobj));
 		return NOTHING;
 	} else {
 		return dobj;
@@ -133,7 +129,7 @@ do_open(int descr, dbref player, const char *direction, const char *linkto)
 		notify(player, "Permission denied. (you don't control the location)");
 		return;
 	} else if (!payfor(player, tp_exit_cost)) {
-		notify_fmt(player, "Sorry, you don't have enough %s to open an exit.", tp_pennies);
+		notifyf(player, "Sorry, you don't have enough %s to open an exit.", tp_pennies);
 		return;
 	} else {
 		/* create the exit */
@@ -152,14 +148,13 @@ do_open(int descr, dbref player, const char *direction, const char *linkto)
 		DBDIRTY(loc);
 
 		/* and we're done */
-		snprintf(buf, sizeof(buf), "Exit %s opened as #%d.", NAME(exit), exit);
-		notify(player, buf);
+		notifyf(player, "Exit %s opened as #%d.", NAME(exit), exit);
 
 		/* check second arg to see if we should do a link */
 		if (*qname != '\0') {
 			notify(player, "Trying to link...");
 			if (!payfor(player, tp_link_cost)) {
-				notify_fmt(player, "You don't have enough %s to link.", tp_pennies);
+				notifyf(player, "You don't have enough %s to link.", tp_pennies);
 			} else {
 				ndest = link_exit(descr, player, exit, (char *) qname, good_dest);
 				DBFETCH(exit)->sp.exit.ndest = ndest;
@@ -175,8 +170,7 @@ do_open(int descr, dbref player, const char *direction, const char *linkto)
 	if (*rname) {
 		PData mydat;
 
-		snprintf(buf, sizeof(buf), "Registered as $%s", rname);
-		notify(player, buf);
+		notifyf(player, "Registered as $%s", rname);
 		snprintf(buf, sizeof(buf), "_reg/%s", rname);
 		mydat.flags = PROP_REFTYP;
 		mydat.data.ref = exit;
@@ -191,7 +185,7 @@ _link_exit(int descr, dbref player, dbref exit, char *dest_name, dbref * dest_li
 	int prdest;
 	dbref dest;
 	int ndest, error;
-	char buf[BUFFER_LEN], qbuf[BUFFER_LEN];
+	char qbuf[BUFFER_LEN];
 
 	prdest = 0;
 	ndest = 0;
@@ -224,10 +218,8 @@ _link_exit(int descr, dbref player, dbref exit, char *dest_name, dbref * dest_li
 		case TYPE_ROOM:
 		case TYPE_PROGRAM:
 			if (prdest) {
-				snprintf(buf, sizeof(buf),
-						"Only one player, room, or program destination allowed. Destination %s ignored.",
+				notifyf(player, "Only one player, room, or program destination allowed. Destination %s ignored.",
 						unparse_object(player, dest));
-				notify(player, buf);
 
 				if(dryrun)
 					error = 1;
@@ -242,10 +234,8 @@ _link_exit(int descr, dbref player, dbref exit, char *dest_name, dbref * dest_li
 			break;
 		case TYPE_EXIT:
 			if (exit_loop_check(exit, dest)) {
-				snprintf(buf, sizeof(buf),
-						"Destination %s would create a loop, ignored.",
+				notifyf(player, "Destination %s would create a loop, ignored.",
 						unparse_object(player, dest));
-				notify(player, buf);
 				
 				if(dryrun)
 					error = 1;
@@ -267,8 +257,7 @@ _link_exit(int descr, dbref player, dbref exit, char *dest_name, dbref * dest_li
 			if (dest == HOME) {
 				notify(player, "Linked to HOME.");
 			} else {
-				snprintf(buf, sizeof(buf), "Linked to %s.", unparse_object(player, dest));
-				notify(player, buf);
+				notifyf(player, "Linked to %s.", unparse_object(player, dest));
 			}
 		}
 		
@@ -370,13 +359,13 @@ do_link(int descr, dbref player, const char *thing_name, const char *dest_name)
 		/* handle costs */
 		if (OWNER(thing) == OWNER(player)) {
 			if (!payfor(player, tp_link_cost)) {
-				notify_fmt(player, "It costs %d %s to link this exit.",
+				notifyf(player, "It costs %d %s to link this exit.",
 						   tp_link_cost, (tp_link_cost == 1) ? tp_penny : tp_pennies);
 				return;
 			}
 		} else {
 			if (!payfor(player, tp_link_cost + tp_exit_cost)) {
-				notify_fmt(player, "It costs %d %s to link this exit.",
+				notifyf(player, "It costs %d %s to link this exit.",
 						   (tp_link_cost + tp_exit_cost),
 						   (tp_link_cost + tp_exit_cost == 1) ? tp_penny : tp_pennies);
 				return;
@@ -499,7 +488,7 @@ do_dig(int descr, dbref player, const char *name, const char *pname)
 		return;
 	}
 	if (!payfor(player, tp_room_cost)) {
-		notify_fmt(player, "Sorry, you don't have enough %s to dig a room.", tp_pennies);
+		notifyf(player, "Sorry, you don't have enough %s to dig a room.", tp_pennies);
 		return;
 	}
 	room = new_object();
@@ -521,8 +510,7 @@ do_dig(int descr, dbref player, const char *name, const char *pname)
 	DBDIRTY(room);
 	DBDIRTY(newparent);
 
-	snprintf(buf, sizeof(buf), "Room %s created as #%d.", name, room);
-	notify(player, buf);
+	notifyf(player, "Room %s created as #%d.", name, room);
 
 	strcpyn(buf, sizeof(buf), pname);
 	for (rname = buf; (*rname && (*rname != '=')); rname++) ;
@@ -549,8 +537,7 @@ do_dig(int descr, dbref player, const char *name, const char *pname)
 				notify(player, "Permission denied.  Parent set to default.");
 			} else {
 				moveto(room, parent);
-				snprintf(buf, sizeof(buf), "Parent set to %s.", unparse_object(player, parent));
-				notify(player, buf);
+				notifyf(player, "Parent set to %s.", unparse_object(player, parent));
 			}
 		}
 	}
@@ -562,8 +549,7 @@ do_dig(int descr, dbref player, const char *name, const char *pname)
 		mydat.flags = PROP_REFTYP;
 		mydat.data.ref = room;
 		set_property(player, buf, &mydat, 0);
-		snprintf(buf, sizeof(buf), "Room registered as $%s", rname);
-		notify(player, buf);
+		notifyf(player, "Registered as $%s", rname);
 	}
 }
 
@@ -636,8 +622,7 @@ do_prog(int descr, dbref player, const char *name)
 		PUSH(newprog, CONTENTS(player));
 		DBDIRTY(newprog);
 		DBDIRTY(player);
-		snprintf(buf, sizeof(buf), "Entering editor for new program %s.", unparse_object(player, newprog));
-		notify(player, buf);
+		notifyf(player, "Entering editor for new program %s.", unparse_object(player, newprog));
 	} else if (i == AMBIGUOUS) {
 		notify(player, "I don't know which one you mean!");
 		return;
@@ -653,8 +638,7 @@ do_prog(int descr, dbref player, const char *name)
 		PROGRAM_SET_FIRST(i, read_program(i));
 		FLAGS(i) |= INTERNAL;
 		PLAYER_SET_CURR_PROG(player, i);
-		snprintf(buf, sizeof(buf), "Entering editor for %s.", unparse_object(player, i));
-		notify(player, buf);
+		notifyf(player, "Entering editor for %s.", unparse_object(player, i));
 		/* list current line */
 		do_list(player, i, NULL, 0);
 		DBDIRTY(i);
@@ -694,7 +678,7 @@ do_edit(int descr, dbref player, const char *name)
 	FLAGS(i) |= INTERNAL;
 	PROGRAM_SET_FIRST(i, read_program(i));
 	PLAYER_SET_CURR_PROG(player, i);
-	notify(player, "Entering editor.");
+	notifyf(player, "Entering editor for %s.", unparse_object(player, i));
 	/* list current line */
 	do_list(player, i, NULL, 0);
 	FLAGS(player) |= INTERACTIVE;
@@ -759,7 +743,6 @@ copy_props(dbref player, dbref source, dbref destination, const char *dir)
 {
 	char propname[BUFFER_LEN];
 	char buf[BUFFER_LEN];
-	char buf2[BUFFER_LEN];
 	PropPtr propadr, pptr;
 
 	/* loop through all properties in the current propdir */
@@ -771,8 +754,7 @@ copy_props(dbref player, dbref source, dbref destination, const char *dir)
 
 		/* notify player */
 		if(tp_verbose_clone && Wizard(OWNER(player))) {
-			snprintf(buf2, sizeof(buf2), "copying property %s", buf);
-			notify(player, buf2);
+			notifyf(player, "copying property %s", buf);
 		}
 
 		/* copy this property */
@@ -798,7 +780,6 @@ copy_props(dbref player, dbref source, dbref destination, const char *dir)
 void
 do_clone(int descr, dbref player, char *name)
 {
-	static char buf[BUFFER_LEN];
 	dbref  thing, clonedthing;
 	int    cost;
 	struct match_data md;
@@ -855,12 +836,11 @@ do_clone(int descr, dbref player, char *name)
 	}
 	
 	if (!payfor(player, cost)) {
-		notify_fmt(player, "Sorry, you don't have enough %s.", tp_pennies);
+		notifyf(player, "Sorry, you don't have enough %s.", tp_pennies);
 		return;
 	} else {
 		if(tp_verbose_clone) {
-			snprintf(buf, sizeof(buf), "Now cloning %s...", unparse_object(player, thing));
-			notify(player, buf);
+			notifyf(player, "Now cloning %s...", unparse_object(player, thing));
 		}
 		
 		/* create the object */
@@ -892,8 +872,7 @@ do_clone(int descr, dbref player, char *name)
 		DBDIRTY(player);
 
 		/* and we're done */
-		snprintf(buf, sizeof(buf), "Object %s cloned as #%d.", NAME(thing), clonedthing);
-		notify(player, buf);
+		notifyf(player, "Object %s cloned as #%d.", NAME(thing), clonedthing);
 		DBDIRTY(clonedthing);
 	}
 	
@@ -942,7 +921,7 @@ do_create(dbref player, char *name, char *acost)
 		cost = tp_object_cost;
 	}
 	if (!payfor(player, cost)) {
-		notify_fmt(player, "Sorry, you don't have enough %s.", tp_pennies);
+		notifyf(player, "Sorry, you don't have enough %s.", tp_pennies);
 		return;
 	} else {
 		/* create the object */
@@ -973,15 +952,13 @@ do_create(dbref player, char *name, char *acost)
 		DBDIRTY(player);
 
 		/* and we're done */
-		snprintf(buf, sizeof(buf), "Object %s created as #%d.", name, thing);
-		notify(player, buf);
+		notifyf(player, "Object %s created as #%d.", name, thing);
 		DBDIRTY(thing);
 	}
 	if (*rname) {
 		PData mydat;
 
-		snprintf(buf, sizeof(buf), "Registered as $%s", rname);
-		notify(player, buf);
+		notifyf(player, "Registered as $%s", rname);
 		snprintf(buf, sizeof(buf), "_reg/%s", rname);
 		mydat.flags = PROP_REFTYP;
 		mydat.data.ref = thing;
@@ -1132,7 +1109,7 @@ do_action(int descr, dbref player, const char *action_name, const char *source_n
 	if (((source = parse_source(descr, player, qname)) == NOTHING))
 		return;
         if (!payfor(player, tp_exit_cost)) {
-                notify_fmt(player, "Sorry, you don't have enough %s to make an action.", tp_pennies);
+                notifyf(player, "Sorry, you don't have enough %s to make an action.", tp_pennies);
                 return;
         }
 
@@ -1146,15 +1123,13 @@ do_action(int descr, dbref player, const char *action_name, const char *source_n
 	FLAGS(action) = TYPE_EXIT;
 
 	set_source(player, action, source);
-	snprintf(buf, sizeof(buf), "Action %s created as #%d and attached.", NAME(action), action);
-	notify(player, buf);
+	notifyf(player, "Action %s created as #%d and attached.", NAME(action), action);
 	DBDIRTY(action);
 
 	if (*rname) {
 		PData mydat;
 
-		snprintf(buf, sizeof(buf), "Registered as $%s", rname);
-		notify(player, buf);
+		notifyf(player, "Registered as $%s", rname);
 		snprintf(buf, sizeof(buf), "_reg/%s", rname);
 		mydat.flags = PROP_REFTYP;
 		mydat.data.ref = action;

src/debugger.c
@@ -208,7 +208,6 @@ unparse_breakpoint(struct frame *fr, int brk)
 void
 muf_backtrace(dbref player, dbref program, int count, struct frame *fr)
 {
-	char buf[BUFFER_LEN];
 	char buf2[BUFFER_LEN];
 	char buf3[BUFFER_LEN];
 	char *ptr;
@@ -238,8 +237,7 @@ muf_backtrace(dbref player, dbref program, int count, struct frame *fr)
 			cnt++;
 		} while (pinst == lastinst && j > 1);
 		if (cnt > 1) {
-			snprintf(buf, sizeof(buf), "     [repeats %d times]", cnt);
-			notify_nolisten(player, buf, 1);
+			notifyf_nolisten(player, "     [repeats %d times]", cnt);
 		}
 		lev = fr->system.top - j;
 		if (ptr) {
@@ -277,8 +275,7 @@ muf_backtrace(dbref player, dbref program, int count, struct frame *fr)
 			ptr = buf2;
 		}
 		if (pinst != lastinst) {
-			snprintf(buf, sizeof(buf), "\033[1;33;40m%3d)\033[0m \033[1m%s(#%d)\033[0m %s:", lev, NAME(ref), ref, ptr);
-			notify_nolisten(player, buf, 1);
+			notifyf_nolisten(player, "\033[1;33;40m%3d)\033[0m \033[1m%s(#%d)\033[0m %s:", lev, NAME(ref), ref, ptr);
 			flag = ((FLAGS(player) & INTERNAL) ? 1 : 0);
 			FLAGS(player) &= ~INTERNAL;
 			list_proglines(player, ref, fr, pinst->line, 0);
@@ -780,9 +777,9 @@ muf_debugger(int descr, dbref player, dbref program, const char *text, struct fr
 				cnt++;
 			} while (!string_compare(ptr, buf) && j > 0);
 			if (cnt > 1)
-				notify_fmt(player, "     [repeats %d times]", cnt);
+				notifyf(player, "     [repeats %d times]", cnt);
 			if (string_compare(ptr, buf))
-				notify_fmt(player, "%3d) %s", j + 1, ptr);
+				notifyf(player, "%3d) %s", j + 1, ptr);
 		}
 		notify_nolisten(player, "*done*", 1);
 		add_muf_read_event(descr, player, program, fr);
@@ -853,10 +850,9 @@ muf_debugger(int descr, dbref player, dbref program, const char *text, struct fr
 			for (i = startline; i <= endline; i++) {
 				pinst = linenum_to_pc(program, i);
 				if (pinst) {
-					snprintf(buf, sizeof(buf), "line %d: %s", i, (i == fr->pc->line) ?
+					notifyf_nolisten(player, "[]line %d: %s", i, (i == fr->pc->line) ?
 							show_line_prims(fr, program, fr->pc, STACK_SIZE, 1) :
 							show_line_prims(fr, program, pinst, STACK_SIZE, 0));
-					notify_nolisten(player, buf, 1);
 				}
 			}
 		} else {
@@ -877,8 +873,7 @@ muf_debugger(int descr, dbref player, dbref program, const char *text, struct fr
 			fr->brkpt.showstack = 0;
 			notify_nolisten(player, "Trace turned off.", 1);
 		} else {
-			snprintf(buf, sizeof(buf), "Trace is currently %s.", fr->brkpt.showstack ? "on" : "off");
-			notify_nolisten(player, buf, 1);
+			notifyf_nolisten(player, "[]Trace is currently %s.", fr->brkpt.showstack ? "on" : "off");
 		}
 		return 0;
 	} else if (!string_compare(cmd, "words")) {

commit 6f0c14314b3aa4f02b77e87996700bf51f43d030

Fix MUF error messages not logging to .debug
Fixes a bug introduced way back in e35d6543.

src/interp.c
@@ -1433,8 +1433,9 @@ interp_err(dbref player, dbref program, struct inst *pc,
                          NAME(OWNER(origprog)));
     }
 
-    notifyf_nolisten(player, "\033[1m%s(#%d), line %d; %s: %s\033[0m",
+    snprintf(buf, sizeof(buf), "\033[1m%s(#%d), line %d; %s: %s\033[0m",
                      NAME(program), program, pc ? pc->line : -1, msg1, msg2);
+    notify_nolisten(player, buf, 1);
 
     lt = time(NULL);
     strftime(tbuf, 32, "%c", localtime(&lt));